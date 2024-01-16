13398276c232a8abecaf8c3f2efd9f30e4ecd59b
siusiusiublack1 <siusiusiublack@gmail.com>
2024-01-16 23:11:16 +0800

    config tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex-row'>
+      <div className='sm:flex hidden mr-10'>
+        Slidebar
+      </div>
+
+      <div className='flex-1 max-sm:w-full max-w-[1280px]
+      mx-auto sm:pr-5'>
+        Navbar
+      </div>
+    </div>
   )
 }
 
