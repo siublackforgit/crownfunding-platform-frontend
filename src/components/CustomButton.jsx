cce49dcb44c89bf1d125e70ca5de64e6018db44d
siusiusiublack1 <siusiusiublack@gmail.com>
2024-01-21 13:27:30 +0800

    mofified navbar

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const CustomButton = () => {
   return (
     <div>CustomButton</div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,11 +8,39 @@ const Navbar = () => {
     const navigate = useNavigate();
     const [isActive, setIsActive] = useState('dashboard');
     const [toggle,settoggle] = useState(false);
+
+    const address = '0xabc'
   return (
     <div className='flex md:flex-row
     flex-col-reverse justify-between mb-[35px] gap-6
     '>
-    Navbar TEST
+        <div className='lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 
+        h-[52px] bg-[#1c1c24]'>
+            <input type="text" placeholder='Search for campaign'
+            className='flex w-full font-epilogue font-normal text-[14px]
+            placeholder:text-[#4b5264] text-white bg-transparent outline-none
+            '
+            />
+            <div className='w-[72px] h-full rounded-[20px] bg-[#4acd8d] 
+            flex justify-center items-center cursor-pointer
+            '>
+                <img src={search} alt="search" 
+                 className='w-[15px] h-[15px] object-contain'    
+                />
+            </div>
+        </div>
+
+        <div className='sm:flex hidden flex-row justify-end gap-4'>
+            <CustomButton 
+            btnType="button"
+            title={address? 'Create a campaign' : 'Connect'}
+            style={address? 'bg-[#1dc071]': 'bg-[#8c6dfd]'}
+            handleClick={()=>{
+                if(address) navigate('create-campaign')
+                else 'connect()'
+            }}
+            />
+        </div>
     </div>
   )
 }
