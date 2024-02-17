c23d43f2f17929682b6b9ac623f9bc7a14394f7b
unknown <rickyyeung@sunnyvision.com>
2024-02-17 20:37:41 +0800

    add CampaignDetail to Route

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,9 @@ const App = () => {
         < NavBar />
         <Routes>
           <Route path="/" element={<Home/>} />
-          <Route path="/create-campaign" element={<CreateCampaign/>} />
-          
           <Route path="/profile" element={<Profile/>} />
+          <Route path="/create-campaign" element={<CreateCampaign/>} />
+          <Route path="/campaign-details/:id" element={<CampaignDetail/>}/>
         </Routes>
       </div>
     </div>
