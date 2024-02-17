dc01b83b3305c383535e1c4269d325f9d2835ab0
unknown <rickyyeung@sunnyvision.com>
2024-02-17 20:32:59 +0800

    finished Navbar Destop and mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
 import { Route , Routes } from 'react-router-dom';
-import { Home , Profile , CreateCampaign , CampaignDetail } from './pages/index.js';
-import { Navbar , Sidebar } from './components'; 
+import { SideBar , NavBar } from './components' ;
+import { CampaignDetail, CreateCampaign , Home , Profile } from './pages/index.js';
 
 
 const App = () => {
 
-  console.log(<Home/>);
 
   return (
-    <div className="relative sm:-8 p-4   min-h-screen flex">
-      <div className='sm:flex hidden mr-10 relative'>
-        <Sidebar/>
+    <div className='relative sm:-8 p-4   min-h-screen flex bg-[#13131a]  flex-row'>
+      <div className='hidden  sm:flex mr-10 relative'>
+        < SideBar />
       </div>
-
-      <div className='flex-1 max-sm:w-full max-w-[1280px]
-      mx-auto sm:pr-5'>
-        <Navbar/>
+      <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
+        < NavBar />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/create-campaign" element={<CreateCampaign/>} />
+          
+          <Route path="/profile" element={<Profile/>} />
+        </Routes>
       </div>
-      
-      <Routes>
-        <Route path='/' element={<Home/>}/> 
-        <Route path='/profile' element={<Profile/>}/>
-        <Route path='/create-campaign' element={<CreateCampaign/>}/> 
-      </Routes>
     </div>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export  { default as Navbar }  from './Navbar' ;
-export  { default as Sidebar } from './Sidebar' ; 
+export  { default as NavBar }  from './Navbar' ;
+export  { default as SideBar } from './Sidebar' ; 
 export  { default as CustomButton } from './CustomButton';
