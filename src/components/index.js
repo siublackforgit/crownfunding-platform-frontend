dc223b35320be1a1e55bd182db4219da26739f41
unknown <rickyyeung@sunnyvision.com>
2024-02-18 14:22:04 +0800

    FormField initial

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export  { default as NavBar }  from './Navbar' ;
 export  { default as SideBar } from './Sidebar' ; 
 export  { default as CustomButton } from './CustomButton';
+export  { default as FormField } from './FormField';
