4228b1492e3599eb0dc2571d2c338cb94c1340a2
unknown <rickyyeung@sunnyvision.com>
2024-02-19 13:37:45 +0800

    setup useStatecontext provider

---
+++ src/context/index.jsx
@@ -0,0 +1,54 @@
+import React, { useContext, createContext } from 'react' ;
+
+import { useAddress, useContract, useWallet, useContractWrite }
+from '@thirdweb-dev/react';
+import {ethers} from 'ethers';
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({children}) =>{
+    const { contract } = useContract
+    ('0xFDcaEa3f5A30BD85d1e96219b443fe5e5E38D1a2');
+    const { mutateAsync : createCampaign } =
+    useContractWrite(contract, 'createCampaign');
+
+    const address = useAddress();
+    const connect = useWallet();
+
+    const publishCampaign = async (form) => {
+        try {
+            const data = await createCampaign(
+                [
+                    address,
+                    form.title,
+                    form.description,
+                    form.target,
+                    new Date(form.deadline).getTime(),
+                    form.image
+                ]
+            )
+
+            console.log('contract call success',data)
+        }
+
+        catch (error){
+
+            console.log('error',error)
+
+        }
+    }
+
+    return (
+        < StateContext.Provider
+            value={{ address,
+            contract,
+            createCampaign: publishCampaign,
+            }}
+        >
+         {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext);
+
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router } from 'react-router-dom';
 import {ChainId,ThirdwebProvider} from '@thirdweb-dev/react';
+import { StateContextProvider } from './context/index.jsx';
 import App from './App.jsx';
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <ThirdwebProvider desiredChainId={ChainId.Goerli}>
         <Router>
+            <StateContextProvider>
             <App/>
+            </StateContextProvider>
         </Router>
     </ThirdwebProvider>
 )
